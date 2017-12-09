52cec13bca8e68a8457f4fa58337935e2e89c085
deh3215 <3215009@gmail.com>
2017-12-09 15:15:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/deh3215/okhttp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.deh3215.okhttp;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    int flag=0;
+    String downLoadStr="http://od.moi.gov.tw/api/v1/rest/datastore/A01010000C-000674-011";
+
+    //=========================================
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PackageManager m = getPackageManager();
+        String s = getPackageName();
+        String path="test.csv";
+        File uploadedFile = new File("/data/data/"+s+"/files/"+path);
+        Log.d("FilePath", "/data/data/"+s+"/files/"+path);
+        String str;
+        if(uploadedFile.exists()) {
+            str = "File Exist";
+            flag=1;
+        }   else
+            str = "File Not Exist !";
+        Log.d("FileInfo", str);
+
+        if(flag == 0) {
+            try {
+                downloadFileAsync(downLoadStr);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void downloadFileAsync(final String downloadUrl) throws Exception {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(downloadUrl).build();
+
+            client.newCall(request).enqueue(new Callback() {
+                String path = "test.csv";
+
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                    Log.d("Test", "Failure");
+                }
+
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (!response.isSuccessful()) {
+                        throw new IOException("Failed to download file: " + response);
+                    }
+                    Log.d("Test", "Success");
+                    //FileOutputStream fos = new FileOutputStream(path);
+                    FileOutputStream fos = openFileOutput(path, Context.MODE_PRIVATE);
+                    fos.write(response.body().bytes());
+                    fos.close();
+                }
+            });
+    }
+}
